4e63a6d61b11e45782603c8606ea69aa9ba516eb
Luis David <luisdaro701@gmail.com>
2026-05-22 16:27:58 -0500

    Delete backend directory

--- backend/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Tarea(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    estimated_time = models.IntegerField() # O el tipo que estés usando
-    actual_time = models.IntegerField(null=True, blank=True)
-    is_completed = models.BooleanField(default=False)
