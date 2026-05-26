27e20158686c6e06c3e64e0daff7362a4a69932f
Luis David <luisdaro701@gmail.com>
2026-05-26 18:36:02 -0500

    Update settings.py

--- backend/timeforge_backend/settings.py
+++ backend/timeforge_backend/settings.py
@@ -80,12 +80,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'anymail.backends.brevo.EmailBackend'
+ANYMAIL = {
+    'BREVO_API_KEY': os.environ.get('BREVO_API_KEY'),
+}
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
