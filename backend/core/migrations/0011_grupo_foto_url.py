b4377fca3775b17fe99ee45f8a1af4129913db34
Luis David <luisdaro701@gmail.com>
2026-05-27 06:27:43 -0500

    Update 0011_grupo_foto_url.py

--- backend/core/migrations/0011_grupo_foto_url.py
+++ backend/core/migrations/0011_grupo_foto_url.py
@@ -1,4 +1,4 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
@@ -8,9 +8,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='grupo',
-            name='foto_url',
-            field=models.TextField(blank=True),
-        ),
+        # Columna agregada manualmente via SQL en pgAdmin
     ]
